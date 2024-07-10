b548add172385a0f4a6e4160f765103543d08742
stream <yuhei.04184132@gmail.com>
2024-07-10 11:15:01 +0900

    雛形を作ってみた

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", function () {
+    var heading = document.createElement("h1");
+    heading.textContent = "Hello, TypeScript with GitHub Pages!";
+    document.body.appendChild(heading);
+});
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const heading = document.createElement("h1");
+    heading.textContent = "Hello, TypeScript with GitHub Pages!";
+    document.body.appendChild(heading);
+  });
+  

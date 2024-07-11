567d5fae5c447579db92ef1d71dd0707b17b2bb0
stream <yuhei.04184132@gmail.com>
2024-07-11 11:25:13 +0900

    page2も作っておく(中身なし)

--- dist/page2.js
+++ dist/page2.js
@@ -1,6 +1,6 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", function () {
     var heading = document.createElement("h1");
-    heading.textContent = "Page 1";
+    heading.textContent = "Page 2";
     document.body.appendChild(heading);
 });
--- src/page2.ts
+++ src/page2.ts
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     const heading = document.createElement("h1");
-    heading.textContent = "Page 1";
+    heading.textContent = "Page 2";
     document.body.appendChild(heading);
 });
